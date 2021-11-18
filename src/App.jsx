5dab9d23eb54e5f13bf520b8643f3c307f2b9717
lubrsy <lubrsy@163.com>
2021-11-18 10:58:00 +0800

    解包前

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+
+class App extends Component {
+  render() {
+    return (
+      <Button variant="contained" color="primary">
+        你好，世界
+      </Button>
+    );
+  }
+}
+
+export default App;
